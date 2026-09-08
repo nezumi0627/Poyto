282a3da0a6213191c774682a4612125aefa78e85
ねずみにうむ <103794002+nezumi0627@users.noreply.github.com>
2026-09-08 15:26:53 +0900

    style: format examples

--- examples/basic.py
+++ examples/basic.py
@@ -1,7 +1,6 @@
 from poyto import PoytoClient
 
-
-with PoytoClient.from_env() as client:
+with PoytoClient() as client:
     print(client.profile())
     for market in client.iter_markets(limit=20):
         print(market.get("id"), market.get("title"))
